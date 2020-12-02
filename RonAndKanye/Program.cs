551338e10f0ddad0b5bc3152948a95226d13da6f
Kyle Scumacher <kschumacher001@gmail.com>
2020-12-02 13:13:38 -0500

    Add project files.

---
+++ RonAndKanye/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RonAndKanye
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            quoteGenerator.GetKanyeQuote();
+            quoteGenerator.GetRonQuote();
+        }
+    }
+}
---
+++ RonAndKanye/quoteGenerator.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace RonAndKanye
+{
+    public class quoteGenerator
+    {
+        public quoteGenerator()
+        {
+
+        }
+        public static void GetKanyeQuote()
+        {
+            var client = new HttpClient();
+            var kanyeURL = "https://api.kanye.rest";
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+            Console.WriteLine($"Kanye: \"{kanyeQuote}\"");
+        }
+        public static void GetRonQuote()
+        {
+            var client = new HttpClient();
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            var ronResponse = client.GetStringAsync(ronURL).Result;
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+            Console.WriteLine($"Ron: {ronQuote}");
+        }
+    }
+}
